95b85ff38476fb8a10835f5d1287d55370ac8ce6
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2024-06-20 16:45:01 +0200

    gitautosync update 15

--- data-carpentry/scripts/start-data.R
+++ data-carpentry/scripts/start-data.R
@@ -39,7 +39,7 @@ interviews[-c(7:131),] # show first 6 rows
 
 # Create a tibble (interviews_100) containing only
 # the data in row 100 of the interviews dataset.
-# 
+interviews_100 <- interviews[100,]
 # Now, continue using interviews for each of the
 # following activities:
 #   
@@ -48,9 +48,9 @@ interviews[-c(7:131),] # show first 6 rows
 # Use that number to pull out just that last row in the tibble.
 # Compare that with what you see as the last row using tail()
   # to make sure it’s meeting expectations.
-# 
+n_rows <- nrow(interviews)
 # Pull out that last row using nrow() instead of the row number.
-# 
+interviews[n_rows,]
 # Create a new tibble (interviews_last) from that last row.
 # 
 # Using the number of rows in the interviews dataset that
