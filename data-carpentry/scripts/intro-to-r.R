f7ecc0069df2e61b40aabd4eda75a2b7ec97414a
peterverhaar <peter.verhaar@gmail.com>
2024-06-20 10:33:01 +0200

    gitautosync update 8

--- data-carpentry/scripts/intro-to-r.R
+++ data-carpentry/scripts/intro-to-r.R
@@ -9,4 +9,19 @@
 
 area_hectares <- 2.0
 
+2.47 * area_hectares
+area_acres <- 2.47 * area_hectares
+print(area_acres)
+area_hectares <- 50
 
+## Exercise
+
+r_width = 5
+r_height = 10
+r_area <- r_width * r_height
+print(r_area)
+
+## Function
+
+a <- sqrt(4)
+pi = 3.1415926
