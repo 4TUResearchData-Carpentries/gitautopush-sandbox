b28152c7302d5f7ed1831baf6743f03af0221d5d
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2024-06-20 16:08:31 +0200

    gitautosync update 5

--- data-carpentry/scripts/start-data.R
+++ data-carpentry/scripts/start-data.R
@@ -4,3 +4,16 @@ library(here)
 
 interviews <- read_csv(here("data/SAFI_clean.csv"))
 read.csv("data/SAFI_clean.csv")
+
+view(interviews)
+head(interviews)
+class(interviews)
+
+dim(interviews) # dimensions of the data frame
+nrow(interviews) # number of rows
+ncol(interviews) # number of columns
+
+tail(interviews) # last 6 rows
+
+names
+
