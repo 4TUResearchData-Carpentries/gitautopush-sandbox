c5c388175e3ec2c20697509d14c85c6d5001f87d
eduardklap <et.klapwijk@gmail.com>
2024-06-21 10:52:34 +0200

    gitautosync update 33

--- data-carpentry/scripts/data-wrangling.R
+++ data-carpentry/scripts/data-wrangling.R
@@ -1,2 +1,6 @@
 # for data wrangling we use two R packages that are part of the Tidyverse:
 # dplyr and tidyr
+# we can load the libraries individually or load the whole tidyverse:
+library(tidyverse)
+# load the here package
+library(here)
