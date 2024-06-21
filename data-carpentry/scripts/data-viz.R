0aa23a7776562a20850f18592b6fa5596579a844
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2024-06-21 15:06:13 +0200

    gitautosync update 29

--- data-carpentry/scripts/data-viz.R
+++ data-carpentry/scripts/data-viz.R
@@ -6,4 +6,15 @@ interviews_plotting <- read_csv(
 )
 
 interviews_plotting %>% 
-  ggplot(aes(x = no_membrs, y = number_items))
+  ggplot(aes(x = no_membrs, y = number_items)) +
+    geom_point(
+      alpha = 0.3
+    )
+
+interviews_plotting %>% 
+  ggplot(aes(x = no_membrs, y = number_items)) +
+    geom_jitter(
+      width = 0.2,
+      height = 0.2
+    )
+
