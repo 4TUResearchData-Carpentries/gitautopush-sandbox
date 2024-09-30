3039d8847946a3ad27e640731201235f97979986
peterverhaar <peter.verhaar@gmail.com>
2024-09-30 15:30:04 +0200

    gitautosync update 9

--- data-carpentry/scripts/start-data.R
+++ data-carpentry/scripts/start-data.R
@@ -5,6 +5,17 @@ library(here)
 ## read CSV
 interviews <- read_csv( here("data","SAFI_clean.csv"),na="NULL")
 
+## first 6 rows
 head(interviews)
 
+## last 6 rows
+tail(interviews)
 
+## number of rows and columns
+dim(interviews)
+
+# Columns names
+names(interviews)
+
+# basic statistical data
+summary(interviews)
