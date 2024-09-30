6dc0264ec983e918c2513dc63f40ef80bd03c351
peterverhaar <peter.verhaar@gmail.com>
2024-09-30 15:35:19 +0200

    gitautosync update 10

--- data-carpentry/scripts/start-data.R
+++ data-carpentry/scripts/start-data.R
@@ -17,5 +17,15 @@ dim(interviews)
 # Columns names
 names(interviews)
 
-# basic statistical data
+# Basic statistical data
 summary(interviews)
+
+## Subsetting
+
+# First row, first column
+interviews[1,1]
+
+# Select row 1 to 3, and columns 5 up and until 6
+interviews[ 1:3 , 5:6 ]
+
+
