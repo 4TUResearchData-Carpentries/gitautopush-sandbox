82556ac61581dd9dbe85815cf20db963dbb80355
Aleksandra Wilczynska <a.e.wilczynska@tudelft.nl>
2024-09-30 10:50:34 +0200

    gitautosync update 47

--- data-carpentry/scripts/intro-to-r.R
+++ data-carpentry/scripts/intro-to-r.R
@@ -69,3 +69,11 @@ possessions <- c(possessions,
                  "mobile phone")
 possessions <- c("car", possessions)
 possessions
+
+
+# Exercise 3 
+
+num_char <- c(1, 2, 3, "a")
+num_logical <- c(1, 2L, 3, TRUE)
+char_logical <- c("a", "b", "c", TRUE)
+tricky <- c(1, 2, 3, "4")
