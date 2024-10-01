9a6470861b932855e23d2505146c0a0de9b97c74
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2024-10-01 14:52:40 +0200

    gitautosync update 37

--- data-carpentry/scripts/data-viz.R
+++ data-carpentry/scripts/data-viz.R
@@ -4,3 +4,13 @@ library(here)
 # load our new dataset that we created at the end of data wrangling
 interviews_plotting <- read_csv(here("data_output/interviews_plotting.csv"))
 
+# Anatomy of a ggplot
+# <DATA> %>%
+#   ggplot(aes(<MAPPINGS>)) +
+#   <GEOM_FUNCTION>() +
+#   <CUSTOMISATION>
+
+interviews_plotting %>% 
+  ggplot(mapping = aes(x = no_membrs, y = number_items))
+
+
