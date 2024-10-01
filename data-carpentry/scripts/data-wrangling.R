dbf51ed094b59b57a1b6087933ac6947ce46238c
eduardklap <et.klapwijk@gmail.com>
2024-10-01 09:33:38 +0200

    gitautosync update 2

--- data-carpentry/scripts/data-wrangling.R
+++ data-carpentry/scripts/data-wrangling.R
@@ -2,3 +2,8 @@
 # dplyr and tidyr
 # we can load the libraries individually or load the whole tidyverse:
 library(tidyverse)
+# load the here package
+library(here)
+
+# first load the SAFI dataset again
+interviews <- read_csv(here("data/SAFI_clean.csv"), na = "NULL")
