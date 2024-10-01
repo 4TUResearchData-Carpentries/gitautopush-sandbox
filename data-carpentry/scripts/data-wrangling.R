d1e154408ba245027e65130cb286e2b7efbaf121
eduardklap <et.klapwijk@gmail.com>
2024-10-01 09:40:15 +0200

    gitautosync update 6

--- data-carpentry/scripts/data-wrangling.R
+++ data-carpentry/scripts/data-wrangling.R
@@ -13,7 +13,7 @@ interviews
 
 # selecting columns and filtering rows ------------
 select(interviews, village, no_membrs, months_lack_food) # select() function from dplyr to select columns from a dataframe
-
+interviews[c("village", "no_membrs", "months_lack_food")] # select the same columns using subsetting
 
 
 
