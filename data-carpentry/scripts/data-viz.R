3383a8d8533727c82538ba17698c360f2f09fc6e
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2024-10-01 14:47:27 +0200

    gitautosync update 36

--- data-carpentry/scripts/data-viz.R
+++ data-carpentry/scripts/data-viz.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(here)
 
+# load our new dataset that we created at the end of data wrangling
 interviews_plotting <- read_csv(here("data_output/interviews_plotting.csv"))
 
