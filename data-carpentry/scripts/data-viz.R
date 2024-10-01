b308d76e6e6a6c1fe71dea6b0645ddca26f70910
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2024-10-01 15:58:39 +0200

    gitautosync update 49

--- data-carpentry/scripts/data-viz.R
+++ data-carpentry/scripts/data-viz.R
@@ -69,11 +69,10 @@ interviews_plotting %>%
   ggplot(aes(x = respondent_wall_type, fill = village)) + # only x variable needed (colour with fill argument)
     geom_bar()
 
-# create a 'dodge'? bar plot
+# create a 'dodge'? parallel? side-by-side? split? bar plot
 interviews_plotting %>% 
   ggplot(aes(x = respondent_wall_type, fill = village)) +
     geom_bar(position = "dodge")
-
 
 
 
